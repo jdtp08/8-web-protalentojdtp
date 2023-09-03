32c847ab9ea94fab1689264144b0f4c6cdbb84c1
Juan Torres <juandiegotorresperez632@gmail.com>
2023-09-03 00:10:41 -0500

    main

---
+++ Replays2/script.js
@@ -0,0 +1,32 @@
+// numerico
+var a = 1;
+
+// texto o string o Cadena
+var cadena = "Hola mundo!";
+
+//Bolean
+
+var camelCase = true;
+var camelCaseNegativo = false;
+
+//flotante o double
+
+var a2 = 1.9;
+
+// conversión entero a flotante
+
+parseFloat (a);
+
+// conversion flotante a entero
+
+parseInt (b);
+
+var animar = {
+    color: "cafe",
+    patas: 4, 
+    nombre: "perro",
+    tieneCola: true
+}
+
+var arreglo = [8,6,5,4];
+var frutas = ["limón", "mango","coco"]
---
+++ operadores-l/index.js
@@ -0,0 +1,8 @@
+var a = 5;
+var b = 5;
+var c = 7;
+var d = 8;
+
+var res = a==b;
+var res1 = a==c;
+console.log (res)
